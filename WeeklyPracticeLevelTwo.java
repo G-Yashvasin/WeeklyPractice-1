8c39c8326b6cabd28b65b0fa03e6df0449fb0583
G-Yashvasin <yg6539@srmist.edu.in>
2026-03-11 01:22:14 +0530

    Q3: Write a program to find the side of the square whose parameter you read from the user

--- WeeklyPracticeLevelTwo.java
+++ WeeklyPracticeLevelTwo.java
@@ -5,18 +5,14 @@ public class WeeklyPracticeLevelTwo {
 
         Scanner sc = new Scanner(System.in);
 
-        double base, height;
+        double perimeter;
 
-        System.out.print("Enter base in cm: ");
-        base = sc.nextDouble();
+        System.out.print("Enter the perimeter: ");
+        perimeter = sc.nextDouble();
 
-        System.out.print("Enter height in cm: ");
-        height = sc.nextDouble();
+        double side = perimeter / 4;
 
-        double areaCm = 0.5 * base * height;
-        double areaIn = areaCm / (2.54 * 2.54);
-
-        System.out.println("The Area of the triangle in sq in is " + areaIn +
-                " and sq cm is " + areaCm);
+        System.out.println("The length of the side is " + side +
+                " whose perimeter is " + perimeter);
     }
 }
