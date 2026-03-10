ff31d6de60923a5655eed118d84c38b6187788b8
G-Yashvasin <yg6539@srmist.edu.in>
2026-03-11 01:57:00 +0530

    Q1: Write a TemperaturConversion program, given the temperature in Celsius as input outputs the temperature in Fahrenheit

---
+++ WeeklyPracticeLevelThree.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class WeeklyPracticeLevelThree {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double celsius;
+
+        System.out.print("Enter temperature in Celsius: ");
+        celsius = sc.nextDouble();
+
+        double fahrenheitResult = (celsius * 9 / 5) + 32;
+
+        System.out.println("The " + celsius + " celsius is " + fahrenheitResult + " fahrenheit");
+    }
+}
